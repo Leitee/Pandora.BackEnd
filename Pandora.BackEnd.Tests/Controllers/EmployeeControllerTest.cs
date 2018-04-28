3c379e8c68d34f8f7134f00191c9dc91efaab3d1
Leonardo <leitee23@hotmail.com>
2018-04-28 09:38:11 -0300

    First Unit Test example was Added

---
+++ Pandora.BackEnd.Tests/Controllers/EmployeeControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Pandora.BackEnd.Api.Controllers;
+using Pandora.BackEnd.Business;
+using Pandora.BackEnd.Business.Contracts;
+using Pandora.BackEnd.Business.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pandora.BackEnd.Tests.Controllers
+{
+    [TestClass]
+    public class EmployeeControllerTest
+    {
+        [TestMethod]
+        public void Get_AllEmployees()
+        {
+            // Arrange
+            var mockService = new Mock<IEmployeeSVC>();
+            mockService
+                .Setup(svc => svc.GetAllAsync()).Returns(Task.FromResult(new BLResponse<List<EmployeeDTO>>()));
+
+            EmployeeController controller = new EmployeeController(mockService.Object, null);
+
+            // Act
+            var resp = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(resp.Result, "Result is null");
+        }
+    }
+}
